fd72ff9d3dafd8c283aa02077980f5e5d581a8ff
kth <rnjsxo1017@gmail.com>
2023-02-20 20:38:06 +0900

    start

---
+++ src/main/java/fadet/P3_GptApi/P3GptApiApplication.java
@@ -0,0 +1,13 @@
+package fadet.P3_GptApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3GptApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3GptApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fadet/P3_GptApi/P3GptApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fadet.P3_GptApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3GptApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
